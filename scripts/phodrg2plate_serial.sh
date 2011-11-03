f5d663d5f2910a5acaca52e7ebc6886ef687d39a
Zachary Moratto <zachary.m.moratto@nasa.gov>
2011-11-03 11:20:37 -0700

    Added some error checking in the PBS scripts for the super computer
    This way it is more clear what exactly failed.

--- scripts/phodrg2plate_serial.sh
+++ scripts/phodrg2plate_serial.sh
@@ -2,5 +2,5 @@
 # Takes the arguments:
 # phodrg2plate <job file prefix> <url>
 
-filename=$1$MPIEXEC_RANK
+filename=$1`echo $MPIEXEC_RANK | awk '{printf("%.2d\n", $1);}'`
 cat $filename | xargs -n1 phodrg2plate $2
--- scripts/phoitalbedo_serial.sh
+++ scripts/phoitalbedo_serial.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 # Takes the arguments:
 # phoitalbedo_serial.sh <number of jobs> <level> <url>
-phoitalbedo -l $2 -j $MPIEXEC_RANK -n $[$1*3] $3
-phoitalbedo -l $2 -j $[$MPIEXEC_RANK+$1] -n $[$1*3] $3
-phoitalbedo -l $2 -j $[$MPIEXEC_RANK+$1+$1] -n $[$1*3] $3
+phoitalbedo -l $2 -j $MPIEXEC_RANK -n $1 $3

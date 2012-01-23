39f0783cb32853290e0f5cc59d2628b9e56f192a
Zachary Moratto <zachary.m.moratto@nasa.gov>
2012-01-23 11:34:58 -0800

    scripts used to compelete apollo combined
    These are the last scripts used to finish the combined apollo mosaic.

--- scripts/phodrg2plate_serial.sh
+++ scripts/phodrg2plate_serial.sh
@@ -3,4 +3,5 @@
 # phodrg2plate <job file prefix> <url>
 
 filename=$1`echo $MPIEXEC_RANK | awk '{printf("%.2d\n", $1);}'`
-cat $filename | xargs -n1 phodrg2plate $2
+echo Opening $filename
+parallel -j-1 phodrg2plate $2 :::: $filename

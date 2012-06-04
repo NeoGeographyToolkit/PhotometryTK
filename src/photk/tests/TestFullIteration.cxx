1e1102e10c9d589563c1095c0370e21573f01247
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2012-06-04 08:14:31 -0700

    Create unit test for albedo

--- src/photk/tests/TestFullIteration.cxx
+++ src/photk/tests/TestFullIteration.cxx
@@ -5,20 +5,16 @@
 // __END_LICENSE__
 
 
+#include <string>
 #include <gtest/gtest.h>
 #include <vw/Image.h>
 #include <vw/FileIO.h>
-#include <photk/ErrorAccumulators.h>
-#include <photk/TimeAccumulators.h>
-#include <photk/AlbedoAccumulators.h>
+#include <photk/Photometry.h>
 #include <test/Helpers.h>
 
 using namespace vw;
-using namespace photk;
-
-// These test is a repeat of previous tests but uses actually data and
-// will run the data into a position where overflow is possible. This
-// will stress that the accumulators are of the correct data type.
+using namespace photometry;
+using namespace std;
 
 template <class ChannelT>
 class FullIterationTest : public ::testing::Test {
@@ -28,22 +24,10 @@ protected:
   typedef PixelGrayA<ChannelT> Px;
 
   virtual void SetUp() {
-    exposure.resize( 4 );
-    drgs.resize( 4 );
-
-    // Load outside images and set exposure
-    for ( int i = 0; i < 4; i++ ) {
-      std::ostringstream ostr;
-      ostr << "input_image" << i+1 << ".tif";
-      read_image( drgs[i], test::SrcName(ostr.str()) );
-      exposure[i] = 1.0;
-    }
-
-    // Build the starting albedo
-    AlbedoInitNRAccumulator<Px > albedo_accu(256,256);
-    for ( int i = 0; i < 4; i++ )
-      albedo_accu( drgs[i], exposure[i] );
-    albedo = albedo_accu.result();
+    std::cout << " --------------- Now in setup" << std::endl;
+    albedo.set_size(255, 255);
+    albedo(128,128) = Px(129,255);
+    albedo(128,129) = Px(120,255);
   }
 
   std::vector<double> exposure;
@@ -56,77 +40,46 @@ typedef FullIterationTest<uint8> FullU8IterationTest;
 typedef FullIterationTest<float> FullF32IterationTest;
 
 TEST_F( FullU8IterationTest, FullCycle ) {
-  EXPECT_PIXEL_EQ( Px(129,255), albedo(128,128) );
 
-  // Calculate current error
-  double starting_error = 0;
-  for ( int i = 0; i < 4; i++ ) {
-    ErrorNRAccumulator eaccum( exposure[i] );
-    for_each_pixel( drgs[i], albedo, eaccum );
-    starting_error += eaccum.value();
+  system("pwd");
+  
+  // Make symbolic links in the current directory to the script and data necessary to run this test.
+  string files[] = {
+    "cmpdirs.sh", "meta",
+    "DIM_input_1280mpp_masked", "DIM_input_2560mpp",
+    "DEM_tiles_sub64",
+    "photometry_settings_3.txt", "albedo_gold_3",
+    "photometry_settings_4.txt", "albedo_gold_4"
+  };
+  
+  for (int s = 0 ; s < sizeof(files)/sizeof(string); s++){
+    string cmd = "ln -s ../../../../src/photk/tests/" + files[s] + " .";
+    std::cout << cmd << std::endl;
+    system(cmd.c_str());
   }
+  string cmd = "ln -s ../../../../src/tools/reconstruct.sh .";
+  std::cout << cmd << std::endl;
+  int flag = system(cmd.c_str());
+  
+  // Run test 3
+  cmd="./reconstruct.sh photometry_settings_3.txt curr_3 > output3.txt";
+  std::cout << cmd << std::endl;
+  system(cmd.c_str());
+  // Run a directory comparison and check the status
+  cmd="cmpdirs.sh albedo_gold_3 albedo_curr_3";
+  std::cout << cmd << std::endl;
+  flag = system(cmd.c_str());
+  EXPECT_EQ(flag, 0);
+  
+  // Run test 4
+  cmd="./reconstruct.sh photometry_settings_4.txt curr_4 > output4.txt";
+  std::cout << cmd << std::endl;
+  system(cmd.c_str());
+  // Run a directory comparison and check the status
+  cmd="cmpdirs.sh albedo_gold_4 albedo_curr_4";
+  std::cout << cmd << std::endl;
+  flag = system(cmd.c_str());
+  EXPECT_EQ(flag, 0);
 
-  // Perform single iteration
-  for ( int i = 0; i < 4; i++ ) {
-    TimeDeltaNRAccumulator taccum( exposure[i] );
-    for_each_pixel( drgs[i], albedo, taccum );
-    exposure[i] += taccum.value();
-  }
-  {
-    AlbedoDeltaNRAccumulator<Px> albedo_accum( 256, 256 );
-    for ( uint i = 0; i < 4; i++ )
-      albedo_accum(drgs[i],albedo,exposure[i]);
-    select_channel(albedo,0) += select_channel(albedo_accum.result(),0);
-  }
-
-  // Calculate final error
-  double final_error = 0;
-  for ( int i = 0; i < 4; i++ ) {
-    ErrorNRAccumulator eaccum( exposure[i] );
-    for_each_pixel( drgs[i], albedo, eaccum );
-    final_error += eaccum.value();
-  }
-  ASSERT_LT( final_error, starting_error );
-  ASSERT_LT( exposure[0], 1.0 );
-  ASSERT_GT( exposure[1], 1.0 );
-  ASSERT_LT( exposure[2], 1.0 );
-  ASSERT_GT( exposure[3], 1.0 );
 }
 
-TEST_F( FullF32IterationTest, FullCycle ) {
-  EXPECT_PIXEL_NEAR( Px(0.507843,1.0), albedo(128,128), 1e-5 );
-
-  // Calculate current error
-  double starting_error = 0;
-  for ( int i = 0; i < 4; i++ ) {
-    ErrorNRAccumulator eaccum( exposure[i] );
-    for_each_pixel( drgs[i], albedo, eaccum );
-    starting_error += eaccum.value();
-  }
-
-  // Perform single iteration
-  for ( int i = 0; i < 4; i++ ) {
-    TimeDeltaNRAccumulator taccum( exposure[i] );
-    for_each_pixel( drgs[i], albedo, taccum );
-    exposure[i] += taccum.value();
-  }
-  {
-    AlbedoDeltaNRAccumulator<Px> albedo_accum( 256, 256 );
-    for ( uint i = 0; i < 4; i++ )
-      albedo_accum(drgs[i],albedo,exposure[i]);
-    select_channel(albedo,0) += select_channel(albedo_accum.result(),0);
-  }
-
-  // Calculate final error
-  double final_error = 0;
-  for ( int i = 0; i < 4; i++ ) {
-    ErrorNRAccumulator eaccum( exposure[i] );
-    for_each_pixel( drgs[i], albedo, eaccum );
-    final_error += eaccum.value();
-  }
-  ASSERT_LT( final_error, starting_error );
-  ASSERT_LT( exposure[0], 1.0 );
-  ASSERT_GT( exposure[1], 1.0 );
-  ASSERT_LT( exposure[2], 1.0 );
-  ASSERT_GT( exposure[3], 1.0 );
-}
